8e6ac588f8afe0b437d8c915d41f92bb06113df4
Rafael Gouveia <raffagouveiabarros@gmail.com>
2023-04-19 14:37:53 -0300

    Farmacia - Exercicio Spring Gen_V1

---
+++ Farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,64 @@
+package com.generation.farmacia.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Nome do produto obrigatório!")
+	private String nome;
+	
+	@NotBlank(message = "Valor do produto obrigatório!")
+	private String preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+	
+	
+}
